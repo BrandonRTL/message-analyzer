d719ebf99ee289566ebf3d5c49e2f4b84927a009
чсм чсм <faitneo@gmail.com>
2022-10-05 19:24:21 +0300

    core

--- core/src/main/java/liga/medical/messageanalyzer/core/CoreApplication.java
+++ core/src/main/java/liga/medical/messageanalyzer/core/CoreApplication.java
@@ -1,14 +1,16 @@
-package liga.medical.core;
+package liga.medical.messageanalyzer.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"liga.medical.messageanalyzer", "liga.medical.common.service"})
 public class CoreApplication {
 
     public static void main(String[] args) {
 
-        //SpringApplication.run(CoreApplication.class, args);
+        SpringApplication.run(CoreApplication.class, args);
         System.out.println("21");
     }
 
---
+++ core/src/test/java/liga/medical/messageanalyzer/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package liga.medical.messageanalyzer.core;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CoreApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
